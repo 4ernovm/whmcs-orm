8a12db9e279a9304987b6d1f05c85bb9a72761aa
chernoff <4ernovm@gmail.com>
2016-02-18 17:06:00 +0200

    Email template model

---
+++ src/Chernoff/ORM/Models/EmailTemplate.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Chernoff\ORM\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class EmailTemplate
+ * @package Chernoff\ORM\Models
+ */
+class EmailTemplate extends Model
+{
+    /** @var string */
+    protected $table = 'tblemailtemplates';
+}
